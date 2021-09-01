19a51597c5a42d99f4e13366e614af6c7c47fbb6
Floyd Hightower <floyd.hightower27@gmail.com>
2021-09-01 17:34:01 -0400

    Fixing lint errors

--- cipher.py
+++ cipher.py
@@ -6,7 +6,7 @@ JOIN_SYMBOL = ','
 
 def _create_encoding_data(s: str) -> dict:
     s += string.printable
-    return {index: char for index, char in enumerate(s)}
+    return dict(enumerate(s))
 
 
 def _get_index_for_char(char: str, encoding_data: dict) -> int:
@@ -15,23 +15,17 @@ def _get_index_for_char(char: str, encoding_data: dict) -> int:
 
 
 def encode(message: str, cipher: str) -> str:
+    """Encode the message with the cipher."""
     encoding_data = _create_encoding_data(cipher)
     encoded_text = [str(_get_index_for_char(char, encoding_data)) for char in message]
     return JOIN_SYMBOL.join(encoded_text)
 
 
 def decode(message: str, cipher: str) -> str:
+    """Decode the message with the cipher."""
     encoding_data = _create_encoding_data(cipher)
     decoded_message = ''
 
     for index in message.split(JOIN_SYMBOL):
         decoded_message += encoding_data[int(index)]
     return decoded_message
-
-
-# encoding_data = create_encoding_data(s)
-# message = "Hello, world!$"
-
-# a = encode(message, encoding_data)
-# print(a)
-# print(decode(a, encoding_data))
